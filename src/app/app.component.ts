7e56627caabb4729f776b2a0c0c6989790e3b1cc
olivercv <oliver_c_v@hotmail.com>
2023-08-24 11:11:52 -0400

    optimization code use data from  json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ColDef, GridSizeChangedEvent, } from 'ag-grid-community';
 import { Observable } from 'rxjs';
+import patients from './util/patient.json';
 
 @Component({
   selector: 'app-root',
@@ -15,21 +16,10 @@ export class AppComponent {
   scrollMode = 'vertical';
   objRowDataStr: any;
   objColDefsStr: any;
-  wrapperStyles = 'width:100%; height: 30%;';
 
   // patients json data 
-  rowData: any[] = [
-    { patientName: 'Francis, Henry', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime: '2020-05-30T10:01:00' , status: 'Ready for Provider 9:26 AM', wait: '00:05' },
-    { patientName: 'Murphy, Catrine', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime:'2015-04-21T16:30:00' , status: '', wait: '' },
-    { patientName: 'Murphy, Alicia', resource: 'Anna Bates', apptType: 'Sick Visit', apptTime:'2010-02-19T12:02:00' , status: 'Checked in 9:57 AM', wait: '00:05' },
-    { patientName: 'Murphy, Alicia', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: '', wait: '' },
-    { patientName: 'Francis, Henry', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: 'In Re-schedule Queue', wait: '' },
-    { patientName: 'Pandy, Peggy', resource: 'Mc Coy Henry', apptType: 'CQM New', apptTime:'1995-10-04T03:27:00' , status: '', wait: '' },
-    { patientName: 'Murphy, Alicia', resource: 'Anna Bates', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: 'Checked in 9:57 AM', wait: '00:05' },
-    { patientName: 'Murphy, Alicia', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: '', wait: '' },
-    { patientName: 'Murphy, Alicia', resource: 'Anna Bates', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: 'Checked in 9:57 AM', wait: '00:05' },
-    { patientName: 'Murphy, Alicia', resource: 'Mc Coy Henry', apptType: 'Sick Visit', apptTime:'1995-10-04T03:27:00' , status: '', wait: '' }
-  ];
+  rowData: any[] = patients;
+  
   // configuration grid json data
 
   colDefs: ColDef[] = [
@@ -55,12 +45,10 @@ export class AppComponent {
   constructor(private http: HttpClient) {
       // change data from http api
       // this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
-      
       // convert Json format to String input textbox
       this.objRowDataStr = JSON.stringify((this.rowData));
       this.objColDefsStr= JSON.stringify((this.colDefs));
       
-
   }
 
   // default column configuration
